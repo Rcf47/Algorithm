daa7bc02bf4d76634287ac87f171e7a1c3b90bd8
Vadim Cherepanov <xenos26@gmail.com>
2023-09-21 04:14:10 +0700

    feat: add implementation merge sort

---
+++ practice/algorithmTraning/mergeSort.ts
@@ -0,0 +1,28 @@
+export function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]): number[] {
+  const result: number[] = [];
+  let l = 0;
+  let r = 0;
+  while (l < left.length && r < right.length) {
+    if (left[l] < right[r]) {
+      result.push(left[l]);
+      l++;
+    } else {
+      result.push(right[r]);
+      r++;
+    }
+  }
+  return result.concat(left.slice(l)).concat(right.slice(r));
+}
+
+console.log(mergeSort([4, 2, 5, 6, 2, 1, 3, 9, 1, 4]));
