79aeb187a000628cc17a363aac9137e3dd104d23
Vadim Cherepanov <xenos26@gmail.com>
2023-09-20 22:20:45 +0700

    feat: add selection sort implementation

---
+++ practice/algorithmTraning/selectionSort.ts
@@ -0,0 +1,18 @@
+export function executeSelectionSort(arr: number[]): number[] | Error {
+  if (!Array.isArray(arr)) {
+    throw new Error("is not Array");
+  }
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
